274537dc0f4fe2175fad627f23276c7e912ed5e4
avulag <GnanaTejaswini.Avula@cdk.com>
2020-05-11 14:26:27 +0530

    Props

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,20 @@
 import React,{ Component } from 'react';
 import PROJECTS from './data/projects.js'
 
+class Project extends Component{
+    render(){
+        const {title,image,description,link}=this.props.project;
+        return(
+           <div style={{display:'inline-block' ,width:500 , margin:10}}>
+               <h3>{title}</h3>
+               <img src={image} alt='profile' style={{width:200,height:120}}/>
+               <p>{description}</p>
+               <a href={link}>{link}</a>
+
+           </div>
+        )
+    }
+}
 class Projects extends Component{
     render(){
         return(
@@ -9,9 +23,7 @@ class Projects extends Component{
                 <div>{
                     PROJECTS.map(PROJECT=>{
                         return(
-                            <div key={PROJECT.id}>
-                                {PROJECT.title}
-                            </div>
+                            <Project key={PROJECT.id} project={PROJECT} />
                         );
                     })
                 }
